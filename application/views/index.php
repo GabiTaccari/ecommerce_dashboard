99f60ce504cd85048a17b9657c20dd5993b79760
Gabriela Gonçalves Taccari <gabriela.goncalves.taccari@gmail.com>
2021-04-28 15:38:26 -0300

    Ajustes iniciais de layout

---
+++ application/views/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php 
+		include 'layout/head.php'; 
+	?>
+</head>
+
+<body>
+	<div class="wrapper">
+    <?php 
+		include 'layout/header.php'; 
+        include 'layout/sidebar.php'; 
+	?>
+
+    conteudo da pagina
+
+    <?php 
+		include 'layout/scripts.php'; 
+	?>
+
+    
